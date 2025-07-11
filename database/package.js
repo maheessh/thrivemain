23774fb91fa94dc14a29ea871fbd885f40ceef20
maheesssh <maheesssh@gmai.com>
2025-07-11 13:49:13 -0400

    Updates of Packages

---
+++ database/package.js
@@ -0,0 +1,50 @@
+// --- YOUR LOCAL DATABASE ---
+// This is the single source of truth for your package data.
+// Simply add, remove, or edit the objects in this 'packages' array.
+// The webpage will automatically update.
+const packages = [
+{
+        name: "Daniel A. Ojerinde",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "Today, 1:41 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "USPS",
+        icon: "truck",  
+        iconBgColor: "bg-blue-100",
+        iconTextColor: "text-blue-500"
+    },
+    {
+        name: "Justine Weng",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "Today, 1:40 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Target",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-red-500"
+    },
+    {
+        name: "Orfi Cortes",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "Today, 1:36 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Unknown",
+        icon: "package", 
+        iconBgColor: "bg-gray-100",
+        iconTextColor: "text-gray-500"
+    },
+    {
+        name: "Zarina Devletova",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "Today, 1:35 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Unknown",
+        icon: "package", 
+        iconBgColor: "bg-gray-100",
+        iconTextColor: "text-gray-500"
+    }
+];
