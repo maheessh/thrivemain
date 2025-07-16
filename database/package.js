d052ad00fb7c41b1d2b126172c60c062fe3adc88
maheesssh <maheesssh@gmai.com>
2025-07-16 13:59:01 -0400

    New Packages

--- database/package.js
+++ database/package.js
@@ -3,33 +3,58 @@
 // Simply add, remove, or edit the objects in this 'packages' array.
 // The webpage will automatically update.
 const packages = [
-     {
-        name: "Kabirah Azeez",
+     
+    
+    
+    {
+        name: "Edward McKinnies",
         packageCount: 1,
-        status: "delivered",
-        arrived: "Today",
+        status: "ready", 
+        arrived: "7/16/2025, 1:50 PM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
-        icon: "truck",
-        iconBgColor: "bg-yellow-100",
-        iconTextColor: "text-yellow-500"
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
     },
     {
-        name: "Miranda Castillo",
-        packageCount: 2,
-        status: "ready",
-        arrived: "Today",
+        name: "Jocelyn Lopez",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/16/2025, 1:50 PM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
-        icon: "truck",
-        iconBgColor: "bg-yellow-100",
-        iconTextColor: "text-yellow-500"
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
+   {
+        name: "Eman Mohamed",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/16/2025, 1:50 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
     },
     {
+        name: "Masuzyo Mwanza",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/16/2025, 1:50 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
+   {
         name: "lilli Mohammed",
         packageCount: 1,
         status: "ready",
-        arrived: "Today",
+        arrived: "7/15/2025, 11:42 AM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "truck",
@@ -41,7 +66,7 @@ const packages = [
         name: "Mathias Berglund",
         packageCount: 2,
         status: "ready", 
-        arrived: "Today, 11:42 AM",
+        arrived: "7/15/2025, 11:42 AM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "shopping-bag",   
@@ -52,13 +77,11 @@ const packages = [
         name: "Harmony Thigpen",
         packageCount: 1,
         status: "ready", 
-        arrived: "Today, 11:42 AM",
+        arrived: "7/15/2025, 11:42 AM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "shopping-bag",   
         iconBgColor: "bg-red-100",
         iconTextColor: "text-gray-500"
     },
-   
-   
 ];
