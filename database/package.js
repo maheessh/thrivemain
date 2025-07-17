3641b69e63ae33413dc13afdf6817ff56b0fa5a8
maheesssh <maheesssh@gmai.com>
2025-07-17 15:12:59 -0400

    New Packages

--- database/package.js
+++ database/package.js
@@ -4,7 +4,50 @@
 // The webpage will automatically update.
 const packages = [
      
-    
+    {
+        name: "Mawa Konate",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/17/2025, 2:00 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
+    {
+        name: "Sara Tovar Guacaneme",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/17/2025, 2:00 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
+    {
+        name: "Jason Ferrer",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/17/2025, 2:00 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
+    {
+        name: "Jocelyn Lopez",
+        packageCount: 1,
+        status: "ready", 
+        arrived: "7/17/2025, 2:00 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "shopping-bag",   
+        iconBgColor: "bg-red-100",
+        iconTextColor: "text-gray-500"
+    },
     
     {
         name: "Edward McKinnies",
@@ -17,65 +60,10 @@ const packages = [
         iconBgColor: "bg-red-100",
         iconTextColor: "text-gray-500"
     },
-    {
-        name: "Jocelyn Lopez",
-        packageCount: 1,
-        status: "ready", 
-        arrived: "7/16/2025, 1:50 PM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "shopping-bag",   
-        iconBgColor: "bg-red-100",
-        iconTextColor: "text-gray-500"
-    },
-   {
-        name: "Eman Mohamed",
-        packageCount: 1,
-        status: "ready", 
-        arrived: "7/16/2025, 1:50 PM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "shopping-bag",   
-        iconBgColor: "bg-red-100",
-        iconTextColor: "text-gray-500"
-    },
-    {
-        name: "Masuzyo Mwanza",
-        packageCount: 1,
-        status: "ready", 
-        arrived: "7/16/2025, 1:50 PM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "shopping-bag",   
-        iconBgColor: "bg-red-100",
-        iconTextColor: "text-gray-500"
-    },
-   {
-        name: "lilli Mohammed",
-        packageCount: 1,
-        status: "ready",
-        arrived: "7/15/2025, 11:42 AM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "truck",
-        iconBgColor: "bg-yellow-100",
-        iconTextColor: "text-yellow-500"
-    },
 
     {
         name: "Mathias Berglund",
         packageCount: 2,
-        status: "ready", 
-        arrived: "7/15/2025, 11:42 AM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "shopping-bag",   
-        iconBgColor: "bg-red-100",
-        iconTextColor: "text-gray-500"
-    },
-     {
-        name: "Harmony Thigpen",
-        packageCount: 1,
         status: "ready", 
         arrived: "7/15/2025, 11:42 AM",
         location: 'Main Office "Eaton 283"',
