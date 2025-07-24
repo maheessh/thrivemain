7c464b42bd84fa57b4476e8c9f99317b077e469e
maheesssh <maheesssh@gmai.com>
2025-07-24 14:09:40 -0400

    New Packages

--- database/package.js
+++ database/package.js
@@ -4,35 +4,11 @@
 // The webpage will automatically update.
 const packages = [
      
-   
     {
-        name: "Abigail Hule",
+        name: "Hillary Reyes",
         packageCount: 1,
         status: "ready",
-        arrived: "Today, 6:41 PM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "truck",
-        iconBgColor: "bg-blue-100",
-        iconTextColor: "text-blue-500"
-    },
-    {
-        name: "Jaynesha Maurais",
-        packageCount: 1,
-        status: "ready",
-        arrived: "Today, 6:41 PM",
-        location: 'Main Office "Eaton 283"',
-        sender: "Amazon",
-        icon: "truck",
-        iconBgColor: "bg-blue-100",
-        iconTextColor: "text-blue-500"
-    },
-    
-    {
-        name: "Nubia Morales",
-        packageCount: 1,
-        status: "ready",
-        arrived: "Today, 6:41 PM",
+        arrived: "7/24/2025, 1:41 PM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "truck",
@@ -41,10 +17,10 @@ const packages = [
     },
 
     {
-        name: "Jiebin Fang",
+        name: "Christian Howze",
         packageCount: 1,
         status: "ready",
-        arrived: "Today, 6:41 PM",
+        arrived: "7/24/2025, 1:41 PM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "truck",
@@ -52,10 +28,21 @@ const packages = [
         iconTextColor: "text-blue-500"
     },
     {
-        name: "Rielyn Jackson",
+        name: "Cheryl Neal",
         packageCount: 1,
         status: "ready",
-        arrived: "Today, 6:41 PM",
+        arrived: "7/24/2025, 1:41 PM",
+        location: 'Main Office "Eaton 283"',
+        sender: "Amazon",
+        icon: "truck",
+        iconBgColor: "bg-blue-100",
+        iconTextColor: "text-blue-500"
+    },
+    {
+        name: "James Winston",
+        packageCount: 1,
+        status: "ready",
+        arrived: "7/24/2025, 1:41 PM",
         location: 'Main Office "Eaton 283"',
         sender: "Amazon",
         icon: "truck",
